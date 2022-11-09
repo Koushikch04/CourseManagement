8a26b2aa563bf4c5788bb2620ac46ea6d931b808
Koushik <98294644+Koushikch04@users.noreply.github.com>
2022-11-09 22:48:33 +0530

    Update Teacher.java

--- src/personPackage/Teacher.java
+++ src/personPackage/Teacher.java
@@ -1,17 +1,41 @@
 package personPackage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Scanner;
 
 public class Teacher extends Person{
     private String teacherID;
     private String departmentName;
 
-    public Teacher(String name, String gender, LocalDate dob, String teacherID, String deptName) {
+    private Double salary;
+    private  String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Teacher(String name, String gender, LocalDate dob, String teacherID, String deptName, String title, Double salary) {
         super(name, gender, dob);
         this.teacherID = teacherID;
         this.departmentName = deptName;
+        this.title=title;
+        this.salary=salary;
     }
 
 
@@ -39,32 +63,64 @@ public class Teacher extends Person{
         return ans;
     }
 
+     public  static  void addTeachers() throws SQLException, FileNotFoundException {
+         String url="jdbc:mysql://localhost:3306/lab6";
+         String UserName="root";
+         String PassWord="root1234";
+         Connection con= DriverManager.getConnection(url,UserName,PassWord);
+         Statement st=con.createStatement();
+         String query="create table if not exists Teacher  (ID varchar(10),name varchar(40),deptName varchar(30),gender varchar(30),salary numeric(10,0),dob Date,title varchar(30),primary key(ID))";
+         st.executeUpdate(query);
+         query="insert into Teacher values(?,?,?,?,?,?,?)";
+         PreparedStatement ps=con.prepareStatement(query);
+         Scanner sc=new Scanner(new File("C:\\Users\\chk24\\Downloads\\Instructor.csv"));
+         while(sc.hasNextLine()) {
+             String[] sr=sc.nextLine().split(",");
+             System.out.println(sr[5]);
+             String[] sl = sr[5].split("-");
+             int year=Integer.parseInt(sl[2]);
+             int month=Integer.parseInt(sl[1]);
+             int day=Integer.parseInt(sl[0]);
+
+             ps.setString(1, sr[0]);
+             ps.setString(2, sr[1]);
+             ps.setString(3, sr[2]);
+             ps.setString(4, sr[3]);
+             ps.setDouble(5,Double.parseDouble(sr[4].trim()));
+             ps.setDate(6, Date.valueOf(year+"-"+month+"-"+day));
+             ps.setString(7, sr[6]);
+             ps.executeUpdate();
+         }
+     }
     public static void addTeacher(Teacher teacher) throws SQLException {
-        String url="jdbc:mysql://localhost:3306/lab5";
+        String url="jdbc:mysql://localhost:3306/lab6";
         String UserName="root";
         String PassWord="root1234";
         Connection con= DriverManager.getConnection(url,UserName,PassWord);
-        String query="create table if not exists Teacher  (ID varchar(10),name varchar(40),deptName varchar(30),dob Date,gender varchar(30),primary key(ID))";
+        String query="create table if not exists Teacher  (ID varchar(10),name varchar(40),deptName varchar(30),gender varchar(30),salary numeric(10,0),dob Date,title varchar(30),primary key(ID))";
         Statement st=con.createStatement();
         st.executeUpdate(query);
-        query="insert into Teacher values(?,?,?,?,?)";
+        query="insert into Teacher values(?,?,?,?,?,?,?)";
         PreparedStatement ps=con.prepareStatement(query);
         ps.setString(1, teacher.getTeacherID());
-        ps.setString(2,teacher.getName());
-        ps.setString(3,teacher.getDepartmentName());
-        ps.setDate(4,Date.valueOf(teacher.getDob()));
-        ps.setString(5,teacher.getGender());
+        ps.setString(2, teacher.getName());
+        ps.setString(3, teacher.getDepartmentName());
+        ps.setString(4, teacher.getGender());
+        ps.setDouble(5,teacher.getSalary());
+        ps.setDate(6, Date.valueOf(teacher.getDob()));
+        ps.setString(7, teacher.getTitle());
         ps.executeUpdate();
         System.out.println("Teacher added");
     }
     public static void removeTeacher(Teacher teacher) throws SQLException {
-        String url="jdbc:mysql://localhost:3306/lab5";
+        String url="jdbc:mysql://localhost:3306/lab6";
         String UserName="root";
         String PassWord="root1234";
         Connection con= DriverManager.getConnection(url,UserName,PassWord);
-        String query="delete from Teacher where  ID=?";
+        String query="delete from Teacher where ID=?";
         PreparedStatement ps=con.prepareStatement(query);
         ps.setString(1,teacher.getTeacherID());
         ps.executeUpdate();
+        System.out.println("Teacher removed successfully");
     }
 }
