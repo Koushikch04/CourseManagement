4a9da3737b2d682251d087c6e5d9dee680522e54
KOUSHIK <chk240404@gmail.com>
2022-11-19 16:02:53 +0530

    kfjd

--- src/Main.java
+++ src/Main.java
@@ -10,37 +10,16 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, FileNotFoundException {
-//        if (args.length == 1 && args[0].equals("-h")) {
-//            CommandLineArguments commandLineArguments = new CommandLineArguments();
-//            System.out.println(commandLineArguments);
-//            return;
-//        }
-//        if (args[1].equals("Students")) {
-//            StudentsCommands.connect(args);
-//        } else if (args[1].equals("Teachers")) {
-//            TeachersCommands.connect(args);
-//        }
-
-
-//        ArrayList<Student> ar = Student.SortByDob();
-
-        ArrayList<Student> ar = Student.SortByBranch();
-
-        Iterator<Student> it=ar.iterator();
-        while(it.hasNext())
-        {
-            Student stud= (Student) it.next();
-            System.out.println(stud.getStudID()+" "+stud.getName()+" "+stud.getDob()+" "+stud.getBranch()+" "+stud.getGender());
+    public static void main(String[] args) throws Exception {
+        if (args.length == 1 && args[0].equals("-h")) {
+            CommandLineArguments commandLineArguments = new CommandLineArguments();
+            System.out.println(commandLineArguments);
+            return;
         }
-
-
-//        ArrayList<Teacher> al=Teacher.SortByDob();
-//        Iterator<Teacher> it=al.iterator();
-//        while(it.hasNext())
-//        {
-//            Teacher t1=it.next();
-//            System.out.println(t1.getTeacherID()+" "+t1.getName()+" "+t1.getDepartmentName()+" "+t1.getGender()+" "+t1.getDob());
-//        }
+        if (args[1].equals("Students")) {
+            StudentsCommands.connect(args);
+        } else if (args[1].equals("Teachers")) {
+            TeachersCommands.connect(args);
+        }
     }
 }
